4f40bc9fad28db9ac2c6615017958da418c9e5dd
salbo <bonbers.51243@gmail.com>
2025-12-26 15:21:06 +0900

    feat:新規登録機能の追加(#createの編集)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # 後でSorceryの自動ログイン処理を入れるauto_login?
+      redirect_to dash_boards_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end
