ae1dff9c12275bcd78dd96ccf7dc9d3c14bd16ba
salbo <bonbers.51243@gmail.com>
2025-12-28 16:35:04 +0900

    feat:編集機能実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -22,10 +22,17 @@ class MemosController < ApplicationController
   end
 
   def edit
+    @memo = current_user.memos.find(params[:id])
   end
 
   def update
-    
+    @memo = current_user.memos.find(params[:id])
+    if @memo.update(memo_params)
+      redirect_to memos_path, notice: "メモの更新が成功しました"
+    else
+      flash.now[:alert] = "メモの更新が失敗しました"
+      render :edit, status: :unprocessable_content
+    end
   end
 
   def destroy
