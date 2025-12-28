4703b6861cd13f3621d3a90422636ef75ef334f0
salbo <bonbers.51243@gmail.com>
2025-12-28 12:35:14 +0900

    feat:ログイン状態で未ログイン画面のアクセスはダッシュボードにリダイレクトする

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :require_login, only: %i[landing_page]
+  before_action :redirect_if_logged_in, only: [:landing_page]
   def landing_page
+  end
+
+  private
+  
+  def redirect_if_logged_in
+    redirect_to dash_boards_path if logged_in?
   end
 end
