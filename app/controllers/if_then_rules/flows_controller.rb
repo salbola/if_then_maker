ac2cd5ed5813c6d0538b7fb9bad089ebe8d12971
salbo <bonbers.51243@gmail.com>
2026-01-03 21:10:04 +0900

    feat:ひとまずメモをくらりパラメータとして指定、ifthenruleをnew,createで作る機能追加

--- app/controllers/if_then_rules/flows_controller.rb
+++ app/controllers/if_then_rules/flows_controller.rb
@@ -1,5 +1,6 @@
 class IfThenRules::FlowsController < ApplicationController
   def step1
+     @memos = current_user.memos.order(created_at: :desc)
   end
 
   def step1_submit
--- app/controllers/if_then_rules_controller.rb
+++ app/controllers/if_then_rules_controller.rb
@@ -4,4 +4,26 @@ class IfThenRulesController < ApplicationController
 
   def show
   end
+
+  def new
+  @if_then_rule = current_user.if_then_rules.build(memo_id: params[:memo_id])
+  end
+
+  def create
+    @if_then_rule = current_user.if_then_rules.build(if_then_rule_params)
+
+    if @if_then_rule.save
+      redirect_to @if_then_rule, notice: "If-Thenルールを作成しました"
+    else
+      flash.now[:alert] = "If-Thenルールの作成が失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def if_then_rule_params
+    params.require(:if_then_rule)
+          .permit(:memo_id, :if_condition, :then_action)
+  end
 end
