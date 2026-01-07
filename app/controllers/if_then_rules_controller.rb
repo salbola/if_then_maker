afd9c475109b42530a3d704cf6c49f565a69a4a3
salbo <bonbers.51243@gmail.com>
2026-01-07 19:29:49 +0900

    feat:warningsになった時ボタンでもう一度確認する機能追加

--- app/controllers/if_then_rules_controller.rb
+++ app/controllers/if_then_rules_controller.rb
@@ -15,7 +15,7 @@ class IfThenRulesController < ApplicationController
     
     @if_then_rule_form = IfThenRuleForm.new(if_then_rule_params, user: current_user)
 
-    if @if_then_rule_form.valid? && (@if_then_rule_form.warnings.blank? || params[:ignore_warnings])
+    if @if_then_rule_form.valid? && (@if_then_rule_form.warnings.blank? || params[:commit_type] == "ignore_warnings")
       @if_then_rule_form.save
       redirect_to if_then_rules_path, notice: "If-Thenルールを作成しました"
 
