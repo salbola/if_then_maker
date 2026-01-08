998ce12ead993407422253e98b69aa7861ab935c
salbo <bonbers.51243@gmail.com>
2026-01-08 17:03:12 +0900

    test:フォームオブジェクトのthen項目や新たなサービスのテスト追加

--- spec/forms/if_then_rule_form_spec.rb
+++ spec/forms/if_then_rule_form_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe IfThenRuleForm, type: :model do
   describe "#valid?の本来のバリデーションの役目" do
     it "if_condition と then_action があれば valid" do
       form = IfThenRuleForm.new(
-        { if_condition: "朝起きたら", then_action: "水を飲む" },
+        { if_condition: "朝起きたら", then_action: "水を飲む", memo_id: 1, },
         user: user
       )
       expect(form).to be_valid
@@ -14,7 +14,7 @@ RSpec.describe IfThenRuleForm, type: :model do
 
     it "if_condition が空だと invalid" do
       form = IfThenRuleForm.new(
-        { if_condition: "", then_action: "水を飲む" },
+        { if_condition: "", then_action: "水を飲む", memo_id: 1, },
         user: user
       )
       expect(form).not_to be_valid
@@ -22,7 +22,7 @@ RSpec.describe IfThenRuleForm, type: :model do
 
     it "then_action が空だと invalid" do
       form = IfThenRuleForm.new(
-        { if_condition: "朝起きたら", then_action: "" },
+        { if_condition: "朝起きたら", then_action: "", memo_id: 1, },
         user: user
       )
       expect(form).not_to be_valid
@@ -32,7 +32,7 @@ RSpec.describe IfThenRuleForm, type: :model do
     context "適切な値の時" do
       it "if_condition と then_action が適正ならwarningsが空になる" do
         form = IfThenRuleForm.new(
-          { if_condition: "朝起きたら", then_action: "水を飲む" },
+          { if_condition: "朝起きたら", then_action: "水を飲む", memo_id: 1, },
           user: user
         )
         form.valid?
@@ -42,7 +42,7 @@ RSpec.describe IfThenRuleForm, type: :model do
     context "ifがおかしい場合" do
       it "if_condition によくない文字'常に'があるとwarningsに追加される" do
         form = IfThenRuleForm.new(
-          { if_condition: "常に", then_action: "水を飲む" },
+          { if_condition: "常に", then_action: "水を飲む", memo_id: 1, },
           user: user
         )
         form.valid?
@@ -51,11 +51,26 @@ RSpec.describe IfThenRuleForm, type: :model do
       it "if_condition に同じユーザーですでに同じものがあるとwarningsに追加される" do
         create(:if_then_rule, user: user, memo: memo, if_condition: "常に", then_action: "水を飲む")
         form = IfThenRuleForm.new(
-          { if_condition: "常に", then_action: "水を吐く" },
+          { if_condition: "常に", then_action: "水を吐く" ,memo_id: 1,},
           user: user
         )
         form.valid?
         expect(form.warnings).to be_present
+      end
+    end
+    context "then_がおかしい場合" do
+      it "then_action に不適切な表現がある場合 warnings に追加される" do
+      form = IfThenRuleForm.new(
+        {
+          memo_id: 1,
+          if_condition: "朝起きたら",
+          then_action: "なんか考える"
+        },
+        user: user
+      )
+
+      form.valid?
+      expect(form.warnings).to be_present
       end
     end
   end
