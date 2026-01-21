7de06fa1ea0c199f76e33ba2f077e99981590254
salbo <bonbers.51243@gmail.com>
2026-01-21 20:03:47 +0900

    refactor:不要になったthen_action_warning_checkerの削除

--- app/forms/if_then_rule_form.rb
+++ app/forms/if_then_rule_form.rb
@@ -71,12 +71,12 @@ class IfThenRuleForm
     @warnings += Warnings::ThenNonVerifiableActionChecker.check(then_action)
     @warnings += Warnings::ThenOversizedActionChecker.check(then_action)
     @warnings += Warnings::ThenNegativeExpressionActionChecker.check(then_action)
+    # 以下,他のレコードが関わる構成系
     @warnings += ::IfConditionDuplicateChecker.check(
       user: @current_user,
       if_condition: if_condition,
       exclude_id: @if_then_rule_of_model&.id
     )
-    # @warnings += ::ThenActionWarningChecker.check(then_action)
     @warnings += ::ActiveLimitWarningChecker.check(user: @current_user, status: status, current_rule: @if_then_rule_of_model)
   end
 
