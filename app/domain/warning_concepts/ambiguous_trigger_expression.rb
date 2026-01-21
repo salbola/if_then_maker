7e7ccdaa3533e68a07fc10e038b85ff5c7be56a4
salbo <bonbers.51243@gmail.com>
2026-01-21 10:13:14 +0900

    refactor:不要な文言が混ざっていたので削除

--- app/domain/warning_concepts/ambiguous_trigger_expression.rb
+++ app/domain/warning_concepts/ambiguous_trigger_expression.rb
@@ -6,7 +6,6 @@ module WarningConcepts
         label: "曖昧表現の回避",
         description: "実行条件が曖昧で、行動のきっかけとして再現性が低い状態",
         hint: "「いつも・そのうち」など曖昧な言葉を使っていない ",
-        description: "実行条件が曖昧で、行動のきっかけとして再現性が低い状態です",
         patterns: {
           always_expression: {
             matchers: [ "常に", "つねに" ],
