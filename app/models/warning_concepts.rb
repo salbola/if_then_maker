e3c97dd14b600b3b734641e5de9cd54b42e6bf88
salbo <bonbers.51243@gmail.com>
2026-01-23 22:23:56 +0900

    refactor:ややこしいヘルパーを介さない形のヒント表示へ変更

--- app/models/warning_concepts.rb
+++ app/models/warning_concepts.rb
@@ -1,19 +1,17 @@
 module WarningConcepts
+#全てをコンセプトを取得する
   def self.all
     constants
       .map { |const| const_get(const) }
       .select { |c| c.is_a?(Class) }
   end
-# メソッド名で分けるタイプ
-  def self.if_concepts
-    all.select { |c| c.definition[:target] == :if }
-  end
-
-  def self.then_concepts
-    all.select { |c| c.definition[:target] == :then }
-  end
-# 引数でtarget指定してもってくる
-  def self.for_target(target)
+# 引数でtarget指定してもってくる。targetは:ifや:thenが入る
+  def self.concepts_for(target)
   all.select { |c| c.definition[:target] == target }
+  end
+# targetは:ifや:thenが入る。その種類のwarning_conceptのヒントの中でも有効設定{enabled: true}のものだけ入っている。
+  def self.hints_for(target)
+    concepts_for(target)
+      .select { |c| c.definition.dig(:hint, :enabled) }
   end
 end
