5e67b19ff83dc32c31b174bd8d6844073b2aa5c0
salbo <bonbers.51243@gmail.com>
2026-01-23 12:19:59 +0900

    refactor:helperゆえに不要な記述を削除

--- app/helpers/warning_concepts_helper.rb
+++ app/helpers/warning_concepts_helper.rb
@@ -1,9 +1,9 @@
 module WarningConceptsHelper
-  def self.if_concepts
+  def if_concepts
     WarningConcepts.all.select { |c| c.name.demodulize.start_with?("If") }
   end
 
-  def self.then_concepts
+  def then_concepts
     WarningConcepts.all.select { |c| c.name.demodulize.start_with?("Then") }
   end
 end
