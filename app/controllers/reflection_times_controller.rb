11c86b69c9793bb9b9706c091f77faa613c4f612
salbo <bonbers.51243@gmail.com>
2026-01-26 18:47:50 +0900

    feat:振り返りのタイミングを設定する機能を追加

---
+++ app/controllers/reflection_times_controller.rb
@@ -0,0 +1,14 @@
+class ReflectionTimesController < ApplicationController
+  def edit
+    
+  end
+  def update
+    current_user.update(reflection_time_params)
+    redirect_to dash_boards_path, notice: "振り返りタイミングを設定しました"
+  end
+
+
+    def reflection_time_params
+    params.require(:user).permit(:reflection_time)
+  end
+end
