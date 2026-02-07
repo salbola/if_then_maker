c36a8693be2b8f691564e0df326a654d2b977e45
salbo <bonbers.51243@gmail.com>
2026-02-07 15:34:32 +0900

    feat:タイトルやボディがない場合用の表示ロジック追加

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,4 +3,10 @@ class Memo < ApplicationRecord
   has_many :if_then_rules, dependent: :nullify
   validates :title, presence: :true, length: { maximum: 100 }
   validates :body, length: { maximum: 10_000 }
+
+  def display_for_select
+    title_text = title.present? ? title.truncate(10) : "（無題）"
+    body_text = body.present? ? body.truncate(10) : "(本文はありません)"
+    "#{title_text}：#{body_text}"
+  end
 end
