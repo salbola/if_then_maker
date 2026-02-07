953e4920a1206cd2e53dda840e41d5e904eae7c8
salbo <bonbers.51243@gmail.com>
2026-02-07 16:15:14 +0900

    test:memoのモデルスペック追加

--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Memo, type: :model do
+  let(:user) { User.create }
   describe "バリデーション" do
-    let(:user) { User.create }
 
     it "title があれば有効" do
       memo = build(:memo, title: "テストメモ", user: user)
@@ -18,6 +18,25 @@ RSpec.describe Memo, type: :model do
     it "title が100文字を超えると無効" do
       memo = build(:memo, title: "a" * 101, user: user)
       expect(memo).not_to be_valid
+    end
+  end
+  describe "#display_for_select" do
+    it "titleとbodyを結合して表示する" do
+      memo = build(:memo, title: "タイトル", body: "本文")
+
+      expect(memo.display_for_select).to eq("タイトル - 本文")
+    end
+
+    it "titleがない場合は無題になる" do
+      memo = build(:memo, title: nil, body: "本文")
+
+      expect(memo.display_for_select).to start_with("（無題）")
+    end
+
+    it "bodyがnilでもエラーにならずハイフンだけ残る" do
+      memo = build(:memo, title: "タイトル", body: nil)
+
+      expect(memo.display_for_select).to end_with(" - ")
     end
   end
 end
