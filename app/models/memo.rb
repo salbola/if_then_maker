ce07dc83dc1139825a83a505f586eea24c63b3b9
salbo <bonbers.51243@gmail.com>
2026-02-10 17:12:14 +0900

    feat:タイトルが無題の時に(無題)と各所で表示されるように調整

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -4,9 +4,15 @@ class Memo < ApplicationRecord
   validates :title, length: { maximum: 100 }
   validates :body, length: { maximum: 10_000 }
 
+
+  def display_title
+    title.present? ? title.truncate(20) : "（無題）"
+  end
+
   def display_for_select
     title_text = title.present? ? title.truncate(20) : "（無題）"
     body_text = body.presence&.truncate(10)
     "#{title_text} - #{body_text}"
   end
+
 end
