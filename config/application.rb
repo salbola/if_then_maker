584cce6bd80eb7d150538ba7b4c7f67d7431a0c4
salbo <bonbers.51243@gmail.com>
2026-02-13 12:43:59 +0900

    add:'rails-i18n'導入

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}")]
+    config.i18n.default_locale = :ja
+
     config.generators.system_tests = nil
     config.generators do |g|
       g.skip_routes true
