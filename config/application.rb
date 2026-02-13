4e76cfb3a557d6c95ba920156b6e91c577405f5e
salbo <bonbers.51243@gmail.com>
2026-02-13 12:50:51 +0900

    fix:謎の設定削除

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module Myapp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}")]
     config.i18n.default_locale = :ja
 
     config.generators.system_tests = nil
