d1aaa7ae8e34fe85d99f4c4b774732e9f9452c9b
salbo <bonbers.51243@gmail.com>
2026-02-14 12:46:34 +0900

    add:pundit導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :require_login
+  include Pundit::Authorization
 
   private
 
