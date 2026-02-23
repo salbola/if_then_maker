bf8fe25df835e32aa0b2cb028dcd5ffc4c32cda1
salbo <bonbers.51243@gmail.com>
2026-02-23 12:14:26 +0900

    test:celeniumのurlを環境変数へ

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,5 +4,5 @@ Capybara.register_driver :remote_chrome do |app|
   options.add_argument('headless')
   options.add_argument('disable-gpu')
   options.add_argument('window-size=1680,1050')
-  Capybara::Selenium::Driver.new(app, browser: :remote, url:"http://chrome:4444/wd/hub", capabilities: options)
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
 end
