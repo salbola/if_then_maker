7fdb0d5323c68485f72c67f40e58430298f20012
salbo <bonbers.51243@gmail.com>
2026-02-23 11:47:19 +0900

    add:system_specで使うブラウザのコンテナ追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,6 +73,10 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.before(:each, type: :system) do
-    driven_by(:selenium_chrome_headless)
+    driven_by :remote_chrome
+    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+    Capybara.server_port = 4444
+    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+    Capybara.ignore_hidden_elements = false
   end
 end
