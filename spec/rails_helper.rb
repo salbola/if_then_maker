af01af74488e21eced3030f12a020b1ea816d62c
salbo <bonbers.51243@gmail.com>
2026-02-23 09:54:54 +0900

    test/system_spec用のドライバー設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'capybara/rails'
+require "pundit/rspec"
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -69,6 +72,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:each, type: :system) do
+    driven_by(:selenium_chrome_headless)
+  end
 end
-require 'capybara/rails'
-require "pundit/rspec"
