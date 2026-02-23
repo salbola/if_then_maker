599a54565a09934204cca56e2e199c794d4637bb
salbo <bonbers.51243@gmail.com>
2026-02-23 12:53:59 +0900

    test:一連の流れのテスト追加

--- spec/system/rule_flow_spec.rb
+++ spec/system/rule_flow_spec.rb
@@ -12,6 +12,35 @@ RSpec.describe "Rule flow", type: :system do
 
     expect(page).to have_content("登録が成功しました")
 
+    # # 2. ログイン
+    # visit login_path
+    # expect(page).to have_content("ログイン")
+    # fill_in "user_email", with: "test@example.com"
+    # fill_in "user_password", with: "password"
+    # click_button "commit"
 
+    # expect(page).to have_content("ログインが成功しました")
+
+    # 3. 作成フローに入る
+    click_link "新しくマイルールを作成する"
+    expect(page).to have_content("メモ選択")
+
+    #4 メモの選択or新しいメモ作成
+    click_link "新しくメモを作成する"
+    fill_in "memo_title", with: "朝するといいこと"
+    fill_in "memo_body", with: "筋トレ"
+    click_button "commit"
+
+    expect(page).to have_content("メモの作成が成功しました")
+
+
+    # 4. ルール作成
+    click_link "このメモで新しいマイルールを作る"
+    fill_in "if_then_rule_form_if_condition", with: "朝起きたら"
+    fill_in "if_then_rule_form_then_action", with: "腕立て伏せ3回する"
+    check "commit_type"
+    click_button "button"
+
+    expect(page).to have_content("作成しました")
   end
 end
