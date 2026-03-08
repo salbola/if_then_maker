6b468d062d4ae90e592b929303fad80acecba385
salbo <bonbers.51243@gmail.com>
2026-03-08 12:04:27 +0900

    fix:最初の許可をキャンセルした時にログインへ戻ってキャンセルしましたとなるように

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -10,17 +10,23 @@ class OauthsController < ApplicationController
   # OAuth コールバック処理
   def callback
     provider = params[:provider]
-    if @user = login_from(provider)
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました！"
+    # 認証でキャンセルした時の処理
+    if params[:error] == "access_denied"
+      redirect_to login_path, alert: "#{provider.titleize}からのログインをキャンセルしました"
     else
-      begin
-        @user = create_from(provider)
-        reset_session
-        auto_login(@user)
+    # 通常の処理
+      if @user = login_from(provider)
         redirect_to root_path, notice: "#{provider.titleize}でログインしました！"
-      rescue StandardError => e
-        logger.error "OAuth Error: #{e.message}"
-        redirect_to root_path, alert: "#{provider.titleize}からのログインに失敗しました"
+      else
+        begin
+          @user = create_from(provider)
+          reset_session
+          auto_login(@user)
+          redirect_to root_path, notice: "#{provider.titleize}でログインしました！"
+        rescue StandardError => e
+          logger.error "OAuth Error: #{e.message}"
+          redirect_to root_path, alert: "#{provider.titleize}からのログインに失敗しました"
+        end
       end
     end
   end
